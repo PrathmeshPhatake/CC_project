b8e783862b66c24a7215a3ce4f38b9e71bab15ab
Prathmesh Phatake <prathmeshPhatake4@gmail.com>
2025-04-25 20:53:13 +0530

    this chanegs after ci/cd pipeline

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('this is chanegs after  pushing code in main branch   in Hello World from Node.js!');
+    res.send('this is chanegs after  pushing code in main branch   ');
 });
 
 app.listen(PORT, () => {
